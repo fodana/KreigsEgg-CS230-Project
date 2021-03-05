f41cbc4cb7597ce3a5fd2ec384d6882c723f509e
quintenjeckering <qrj0001@mix.wvu.edu>
2021-03-04 19:07:33 -0500

    Initial Home Page Commit

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php 
+require 'includes/header.php';
+?>
+
+<main>
+<link rel="stylesheet" href="styles/about.css">
+
+<div class= "title">
+    <h1 style="font-size: 25px;color;black"> About KreigsEgg </h1>
+    <p>
+            KreigsEgg.com is an online marketplace that brings buyers, sellers, and enthusiasts of all things computers
+            together. KreigsEgg makes buying and selling of computer hardware and accessories easier than every.
+            Users can create an account for free and can then begin exploring the vast availability of KreigsEgg.
+        </p>
+</div>
+<div class = "buffer">
+
+
+</div><div class = "dev">
+<h3>Developers</h3>
+<p>
+    The KreigsEgg development team consists of 7 members: Dawson Smetana, Michael Tarantino, Dana Foual, Jacob Durgin, Shayna Brewer, Quinelle Richardson,
+    and Quinten Jeckering. Each developer is striving to bring you the most state of the art computer-part buying and selling webpage on the internet.
+</p>
+</div>
+
+</main>
--- index.php
+++ index.php
@@ -4,6 +4,50 @@ require 'includes/header.php';
 
 <main>
 
-    <p>Main Page</p>
+    <link rel="stylesheet" href="styles/index.css">
+
+    <div class="home">
+        <title> Our Mission Statement - Home</title>
+        <h1>Home</h1>
+
+    </div>
+
+    <div class="Left-Column">
+        <div class="card">
+            <div class="nav-list-link">
+                <h3>
+                    Links
+                </h3>
+                <div>
+                    <a href="signup.php"> Create Account</a>
+                </div>
+                <div>
+                    <a href="login.php">Login</a>
+                </div>
+                <div>
+                    <a href="listings.php">Create Listing</a>
+                </div>
+                <div>
+                    <a href="about.php">About Page</a>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+    </div>
+
+    <div class="center-column">
+        <div class="card">
+
+            <h2 style="color:Black"> Welcome to KreigsEgg!</h2>
+            <h3 style="color:NavyBlue"> The forefront of computer part buying and selling </h3>
+
+        </div>
+    </div>
+    </div>
 
 </main>
