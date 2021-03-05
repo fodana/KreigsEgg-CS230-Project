95bf0b5784d7a4f48bd9f2a5d87d81fe0754baa5
MichaelTarantino <mtt0011@mix.wvu.edu>
2021-03-04 22:33:11 -0500

    Glorious Nav Bar update 4.2

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@ session_start();
     <body>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #326273;">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">KreigsEgg</a>
+                <a class="navbar-brand nav-piece" style="color: #FFFFFF" href="../index.php">KreigsEgg</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -36,29 +36,31 @@ session_start();
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                            <a class="nav-link nav-piece" style="color: #FFFFFF" aria-current="page" href="../index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../listings.php">Listings</a>
+                            <a class="nav-link nav-piece" style="color: #EEEEEE" href="../listings.php">Listings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-piece" style="color: #EEEEEE" href="../about.php">About</a>
                         </li>
                         <?php if(isset($_SESSION['uid'])){
                             echo'
                             <li class="nav-item">
-                                <a class="nav-link" href="../profile.php">Profile</a>
+                                <a class="nav-link nav-piece" style="color: #EEEEEE" href="../profile.php">Profile</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="includes/logout.php">Logout</a>
+                                <a class="nav-link nav-piece" style="color: #EEEEEE" href="includes/logout.php">Logout</a>
                             </li>
                             ';
                         }else{
                             echo'
                             <li class="nav-item">
-                                <a class="nav-link" href="../login.php">Login</a>
+                                <a class="nav-link nav-piece" style="color: #EEEEEE" href="../login.php">Login</a>
                             </li>
                             ';
                         }
                         ?>
-                        
                     </ul>
                 </div>
             </div>
