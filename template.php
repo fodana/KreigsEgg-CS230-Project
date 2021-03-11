b41b22054758592641a879de051d5b06616354d1
MichaelTarantino <mtt0011@mix.wvu.edu>
2021-03-10 19:16:27 -0500

    Template Page added

---
+++ template.php
@@ -0,0 +1,15 @@
+<?php
+require 'includes/header.php';
+?>
+
+<main class="outer-bg-cover">
+    <link rel="stylesheet" href="styles/login.css">
+    <div class="container">
+        <div class="inner-bg-cover">
+            <div class="row">
+            <!-- Page Contents Goes in here. This row can be split to organize content -->
+            </div>
+        </div>
+    </div>
+
+</main>
