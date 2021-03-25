c8b31c5fd877b916ccb2d575debe53aa923e1ff0
quinelle <mar13queen@gmail.com>
2021-03-25 18:40:14 -0400

    example support w/ faq

---
+++ support_page_faq.php
@@ -0,0 +1,67 @@
+<?php
+require 'includes/header.php';
+?>
+
+<main class="outer-bg-cover">
+    <link rel="stylesheet" href="styles/support_page_faq.css">
+    <div class="container">
+        <div class="inner-bg-cover">
+            <div class="row">
+                <!-- Page Contents Goes in here. This row can be split to organize content -->
+                <div class="" id="accordion">
+                    <div class="titleHeader">Support Page</div>
+                    <div class="card ">
+                        <div class="titleCard">Buyer</div>
+                        <div class="card-header">
+                            <h4 class="card-header">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseOne">Example</a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="card-block">
+                                Example
+                            </div>
+                        </div>
+                        <div class="card-header">
+                            <h4 class="card-header">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseTwo">Example</a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse in">
+                            <div class="card-block">
+                                Example
+                            </div>
+                        </div>
+                        <div class="card ">
+                            <div class="titleCard">Seller</div>
+                            <div class="card-header">
+                                <h4 class="card-header">
+                                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseThree">Example2</a>
+                                </h4>
+                            </div>
+                            <div id="collapseThree" class="panel-collapse collapse in">
+                                <div class="card-block">
+                                    Example2
+                                </div>
+                            </div>
+                            <div class="card-header">
+                            <h4 class="card-header">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseFour">Example</a>
+                            </h4>
+                        </div>
+                        <div id="collapseFour" class="panel-collapse collapse in">
+                            <div class="card-block">
+                                Example
+                            </div>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+</main>
