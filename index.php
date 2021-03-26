353892d9bbc7fcd4d43df9b7683de89d4e21f36d
jakedurg <jmd0078@mix.wvu.edu>
2021-03-25 23:52:06 -0400

    thursday work

--- index.php
+++ index.php
@@ -1,51 +1,61 @@
-<?php 
-require 'includes/header.php';
-?>
-
-<main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="styles/index.css">
 
-    <div class="home">
-        <title> Our Mission Statement - Home</title>
-        <h1>Home</h1>
+</head>
 
-    </div>
+<body>
+    <main class="outer-bg-cover">
 
-    <div class="Left-Column">
-        <div class="card">
-            <div class="nav-list-link">
-                <h3>
-                    Links
-                </h3>
-                <div>
-                    <a href="signup.php"> Create Account</a>
+        <div class="container">
+            <div class="inner-bg-cover">
+
+                <div class= "row">
+
+
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="1000">
+                        <div class="carousel-inner">
+                        <a href="post.php"><div class="carousel-item active">
+                                <img class="d-block w-100" src="images/accessories.jpg" alt="First slide" >
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Trying to get rid of spare parts?</h5>
+                                    <p>Create a listing today!</p>
+                                </div></a>
+                            </div>
+                            <a href="listings.php"><div class="carousel-item">
+                                <img class="d-block w-100" src="images/gpu.jpg" alt="Second slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Looking for Computer Parts?</h5>
+                                    <p>Check out our Listings!</p>
+                                </div></a>
+                            </div>
+                            <a href="support.php"><div class="carousel-item">
+                                <img class="d-block w-100" src="images/support.jpg" alt="Third slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Need some help?</h5>
+                                    <p>Visit our Support Page!</p>
+                            </div></a>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <a href="login.php">Login</a>
-                </div>
-                <div>
-                    <a href="listings.php">Create Listing</a>
-                </div>
-                <div>
-                    <a href="about.php">About Page</a>
-                </div>
-                <div>
-                    <a href="support.php">Support Page</a>
-                </div>
-
-    </div>
+                
+                <h2>Welcome to KreigsEgg</h2>
 
 
-    </div>
 
-    <div class="center-column">
-        <div class="card">
 
-            <h2 style="color:Black"> Welcome to KreigsEgg!</h2>
-            <h3 style="color:NavyBlue"> The forefront of computer part buying and selling </h3>
+
+
+            </div>
 
         </div>
-    </div>
-    </div>
 
-</main>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+</body>
