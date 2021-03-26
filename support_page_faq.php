a460b3fcec0f0afc8cb8c55810c544809fb213b4
shaynabrewer <slb0088@mix.wvu.edu>
2021-03-25 21:35:23 -0400

    Updated Support Page

--- support_page_faq.php
+++ support_page_faq.php
@@ -11,57 +11,72 @@ require 'includes/header.php';
                 <div class="" id="accordion">
                     <div class="titleHeader">Support Page</div>
                     <div class="card ">
-                        <div class="titleCard">Buyer</div>
+                        <div class="titleCard">FAQ Buyer</div>
                         <div class="card-header">
                             <h4 class="card-header">
                                 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                    href="#collapseOne">Example</a>
+                                    href="#collapseOne">Buyer Question 1</a>
                             </h4>
                         </div>
                         <div id="collapseOne" class="panel-collapse collapse in">
                             <div class="card-block">
-                                Example
+                                Buyer Answer 1
                             </div>
                         </div>
                         <div class="card-header">
                             <h4 class="card-header">
                                 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                    href="#collapseTwo">Example</a>
+                                    href="#collapseTwo">Buyer Question 2</a>
                             </h4>
                         </div>
                         <div id="collapseTwo" class="panel-collapse collapse in">
                             <div class="card-block">
-                                Example
+                                Buyer Answer 2
                             </div>
                         </div>
                         <div class="card ">
-                            <div class="titleCard">Seller</div>
+                            <div class="titleCard">FAQ Seller</div>
                             <div class="card-header">
                                 <h4 class="card-header">
                                     <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                        href="#collapseThree">Example2</a>
+                                        href="#collapseThree">Seller Question 1</a>
                                 </h4>
                             </div>
                             <div id="collapseThree" class="panel-collapse collapse in">
                                 <div class="card-block">
-                                    Example2
+                                    Seller Answer 1
                                 </div>
                             </div>
                             <div class="card-header">
-                            <h4 class="card-header">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                    href="#collapseFour">Example</a>
-                            </h4>
-                        </div>
-                        <div id="collapseFour" class="panel-collapse collapse in">
-                            <div class="card-block">
-                                Example
+                                <h4 class="card-header">
+                                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseFour">Seller Question 2</a>
+                                </h4>
                             </div>
-                        </div>
-                        </div>
+                            <div id="collapseFour" class="panel-collapse collapse in">
+                                <div class="card-block">
+                                    Seller Answer 2
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
+            <div class="contact">
+            <div class="buffer">
+            </div>
+                <h1 style="font-size: 25px;color;black;"> Question not answered? </h1>
+                <h1 style="font-size: 25px;color;black;"> Contact Us </h1>
+                <p>Benjamin M. Statler College of Engineering and Mineral Resources
+                    <br>1374 Evansdale Drive
+                    <br>Morgantown, West Virginia 26506
+                    <br>
+                    <br>Phone: (123) 456-7890
+                    <br>Email: KreigsEgg@gmail.com
+                </p>
+                <div class="buffer">
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
