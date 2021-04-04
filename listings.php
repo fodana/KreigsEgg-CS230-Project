e97707d961af997ebc553e70e11ca84647e3708b
jakedurg <jmd0078@mix.wvu.edu>
2021-04-04 10:25:45 -0400

    minor changes, added what will be an edit button

--- listings.php
+++ listings.php
@@ -1,6 +1,27 @@
 <?php
     require 'includes/header.php';
+    require 'includes/dbhandler.php';
+
+    if(isset($_SESSION['uid'])){
+    $prof_user = $_SESSION['uname'];
+    $sqlpro = "SELECT * FROM profiles WHERE uname='$prof_user';";
+    $res = mysqli_query($conn, $sqlpro);
+    $row = mysqli_fetch_array($res);
+    $admin == FALSE;
+    if($row['admin'] == 1){
+        $admin = TRUE;
+    }
+    }
 ?>
+
+<script>
+    function txtBox(input,sbm) {
+        document.getElementById(input).className = "show";
+        document.getElementById(sbm).className = "sbmshow";
+    }
+</script>
+
+
 
 <main class="outer-bg-cover">
     <link rel="stylesheet" href="styles/listing.css">
@@ -14,7 +35,9 @@
                     <div class="card my-cards" style="width: 22rem;">
                         <img class="card-img-top" src="images/pan.jpeg" alt="Card image cap">
                         <div class="card-body">
+                            
                             <h5 class="card-title">Frying Pan</h5>
+                            <button type="submit" class="admin-edit">test</button>
                             <p class="card-text">Haha look at this cool frying pan</p>
                             <a href="#" class="btn btn-lg def-btn btn-lg">Request Seller Info</a>
                         </div>
