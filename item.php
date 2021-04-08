44d2059d242a77c989006a6bd6baf5a405ffcc74
jakedurg <jmd0078@mix.wvu.edu>
2021-04-08 19:44:21 -0400

    some thursday work

--- item.php
+++ item.php
@@ -14,18 +14,35 @@ $img = $row['imagePath'];
 $desc = $row['Description'];
 $price = $row['Price'];
 
-
+if(isset($_SESSION['uid'])){
+    $uid = $_SESSION['uid'];
+    $sqlpro = "SELECT * FROM profiles WHERE uname='$uid';";
+    $res = mysqli_query($conn, $sqlpro);
+    $row = mysqli_fetch_array($res);
+    $admin = $row['admin'];
+    $uname = $row['uname'];
+}
 
 ?>
 
 <main class="outer-bg-cover">
     <link rel="stylesheet" href="styles/login.css">
+    <script>
+    function txtBox(input,sbm) {
+        document.getElementById(input).className = "show";
+        document.getElementById(sbm).className = "sbmshow";
+    }
+    </script>
     <div class="container">
         <div class="inner-bg-cover">
             <img src=<?php echo $img; ?>/>
-            <h3><?php echo $title; ?></h3>
-            <p><?php echo $price; ?></p>
-            <p><?php echo $desc; ?></p>
+            <form action = "includes/item-helper.php" method="POST">
+                <h3><?php echo $title; ?></h3>
+                <input class="show-btn" type="button" name="show-btn" value="Edit" onclick="txtBox(input,sbm)" />
+                <p><?php echo $price; ?></p>
+                <p><?php echo $desc; ?></p>
+            </form>
+            
 
         </div>
     </div>
