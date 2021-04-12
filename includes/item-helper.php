faddb33b5eff5190d66c9a5c51dae552472283e1
jakedurg <jmd0078@mix.wvu.edu>
2021-04-12 14:31:46 -0400

    forgot to add additional sql updates

--- includes/item-helper.php
+++ includes/item-helper.php
@@ -8,10 +8,13 @@ if(isset($_POST['submit-updates'])){
     $newDesc = $_POST['desc-update'];
     $lid = $_POST['listingID'];
 
-    $sql = "UPDATE listings SET  Price='$newPrice' WHERE lid='$lid'";
+    $sql = "UPDATE listings SET  Title='$newTitle' WHERE lid='$lid'";
+    $sql2 = "UPDATE listings SET  Price='$newPrice' WHERE lid='$lid'";
+    $sql3 = "UPDATE listings SET  Description='$newDesc' WHERE lid='$lid'";
     mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql2);
+    mysqli_query($conn, $sql3);
     header("Location: ../item.php?lid=$lid&success=UpdateSuccess");
     
 } 
 
-//Title='$newTitle', Price='$newPrice', Description='$newDesc'
