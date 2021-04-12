fdb85c44ea20f79eac6647b89d033a1f73578e35
Dawson Smetana <70485276+djs0065@users.noreply.github.com>
2021-04-12 14:12:59 -0400

    Update display-pop.php

--- includes/display-pop.php
+++ includes/display-pop.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
 
-
+// NULL case means empty search
 if($_SESSION['pop'] == NULL){
     Echo '<h5 class="results center-me">No Results, Please try ajusting your search parameters</h5>';
     Echo $_SESSION['uname'];
     $_SESSION['pop'] = "Default";
+// Default search case is all listings new -> old
 }else if($_SESSION['pop'] == "Default"){
     include_once 'includes/dbhandler.php';
     $sql = "SELECT * FROM listings ORDER BY Date DESC";
     //echo $sql;
     $counter = 3;
     $query = mysqli_query($conn, $sql);
-
+    // print results with specific organization, logic makes rows of 3 cards
     echo'<h5 class="results center-me">Search Results</h5>';
     while($row = mysqli_fetch_assoc($query)){
         if($counter == 3){
@@ -49,14 +50,14 @@ if($_SESSION['pop'] == NULL){
         echo'
         </div>
         ';
-
+// Final Case means exisitng search
 }else{
     include_once 'includes/dbhandler.php';
     $sql = $_SESSION['pop'];
     //echo $sql;
     $counter = 3;
     $query = mysqli_query($conn, $sql);
-
+    // print results with specific organization, logic makes rows of 3 cards
     echo'<h5 class="results center-me">Search Results</h5>';
     while($row = mysqli_fetch_assoc($query)){
         if($counter == 3){
@@ -90,4 +91,3 @@ if($_SESSION['pop'] == NULL){
         </div>
         ';
 }
-
