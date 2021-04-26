bdc2315a3004a91f4ecbe75ae71d08307671b486
quintenjeckering <qrj0001@mix.wvu.edu>
2021-04-26 15:01:52 -0400

    Messages Styling Update

--- includes/display-messages.php
+++ includes/display-messages.php
@@ -5,9 +5,18 @@ require 'includes/dbhandler.php';
 $uname = $_SESSION['uname'];
 $sql = "SELECT * FROM messages WHERE receiver='$uname' ORDER BY time DESC";
 $query = mysqli_query($conn, $sql);
-
+echo'<div class = "title">
+            <h3>Messages</h3>
+        </div>';
 while($row = mysqli_fetch_assoc($query)){
     $message = $row['message'];
-    echo "$message";
-
+    
+    
+    echo '
+     <div class = "display">   
+        '.$message.'
+    
+     </div>
+    ';
+   
 }
