61217544c07881639e5dac3e6be7dfd201464c07
MichaelTarantino <70380718+MichaelTarantino@users.noreply.github.com>
2021-04-28 23:35:13 -0400

    Correct stylesheet path

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@ session_start();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
-        <link rel="stylesheet" href="../styles/header.css">
+        <link rel="stylesheet" href="styles/header.css">
 
 </head>
 <header>
